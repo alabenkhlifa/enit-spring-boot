155f8342db13ca1da53d72afeeb553021fab0a20
Ala Ben Khlifa <ala.benkhlifa@maibornwolff.de>
2022-08-13 15:02:04 +0100

    Initial commit

---
+++ src/test/java/tn/enit/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
